f37420188bb2744c7b9c1e627dfe439ad9b869c9
Fedor <fordin2020@gmail.com>
2022-04-06 22:28:33 +0300

    init

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    <div> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Shop">Shop</h1>
+    </div>
+    <div className="Hider"></div>
+    <div className="MainContent"><img src='https://www.meme-arsenal.com/memes/e4e7a2ec2a248891c0e0419f0b1efe01.jpg'/></div>
     </div>
   );
 }

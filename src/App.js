e773dc2a172e8e2b13584757271610b3a9a26fd3
Fedor <fordin2020@gmail.com>
2022-04-10 07:39:30 +0300

    impoved hider animation, removed fun pic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -8,7 +8,16 @@ function App() {
       <h1 className="Shop">Shop</h1>
     </div>
     <div className="Hider"></div>
-    <div className="MainContent"><img src='https://www.meme-arsenal.com/memes/e4e7a2ec2a248891c0e0419f0b1efe01.jpg'/></div>
+    <div className="MainContent"><p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt eget erat fringilla eleifend. Donec gravida condimentum massa et commodo. Curabitur dapibus leo quam, quis vulputate ipsum tempus nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed risus magna, ornare congue vehicula at, finibus malesuada purus. Pellentesque eget semper libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+      Aliquam erat volutpat. Fusce enim nibh, varius eu dictum sit amet, efficitur vel lorem. Nullam vehicula nunc tellus, at convallis ipsum interdum id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc sit amet lacus rhoncus, luctus dolor sit amet, dapibus dolor. Integer eros mi, bibendum vel tincidunt in, laoreet ut diam. Duis interdum congue velit sit amet scelerisque. Morbi ultrices massa et sapien aliquet, id efficitur ligula luctus.
+
+      Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent porttitor, mi et interdum luctus, metus lorem sollicitudin lacus, gravida venenatis ipsum risus egestas est. Aenean commodo vulputate velit, non vulputate elit viverra ut. In id dolor posuere, volutpat nibh ac, consectetur mi. Vestibulum molestie fermentum tempor. Ut laoreet blandit consectetur. Sed ipsum lacus, tempor et suscipit nec, tristique a turpis. Integer sit amet ultrices ex. Integer condimentum maximus nisi et efficitur. Fusce tempor turpis quam, sit amet volutpat ipsum ultricies eget. Etiam quis finibus eros. Cras consequat consequat euismod. Nulla facilisi. Integer a mi consequat, feugiat ante at, convallis leo. In lorem eros, suscipit vel metus ut, gravida laoreet nisl.
+
+      Etiam vitae vulputate est, et feugiat nibh. Proin dapibus mauris quis mattis efficitur. Aenean et purus pretium, mattis nulla et, pulvinar massa. Pellentesque tristique quam eu feugiat placerat. Nulla ipsum libero, lacinia sit amet turpis et, malesuada ultrices tortor. Aliquam erat volutpat. Sed at odio nisl. Pellentesque efficitur pellentesque felis, non consequat mauris interdum a. Praesent pulvinar elit purus, tempus iaculis metus pretium ac. Integer pellentesque nec nibh a tristique. Praesent id diam sodales, luctus diam eu, tempor eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+      In mattis erat risus, nec hendrerit lorem congue non. Aliquam dictum, erat nec elementum congue, nulla risus imperdiet mi, mollis scelerisque quam lorem ut est. Praesent eget nisl mauris. In aliquet purus orci, eget pulvinar ipsum condimentum vel. Donec sit amet magna ut mi finibus blandit. Nam porta magna a lorem tempus, a posuere eros dapibus. Vivamus dapibus lorem ut tellus rutrum, a tempor est bibendum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris nisl lacus, tempor non auctor sed, sollicitudin non neque. Integer placerat ante ligula, ac vestibulum justo pharetra at.</p></div>
     </div>
   );
 }
